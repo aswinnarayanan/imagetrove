b4b8a11906eead738fc00a722aad624d3b49f2a3
Aswin Narayanan <aswin@excelsol.com>
2018-10-21 19:35:37 +1000

    First time run script

---
+++ first-run.sh
@@ -0,0 +1,6 @@
+
+source ~/appenv/bin/activate; python mytardis.py createcachetable default_cache
+source ~/appenv/bin/activate; python mytardis.py createcachetable celery_lock_cache
+source ~/appenv/bin/activate; python mytardis.py makemigrations --noinput
+source ~/appenv/bin/activate; python mytardis.py migrate --noinput
+source ~/appenv/bin/activate; python mytardis.py collectstatic
